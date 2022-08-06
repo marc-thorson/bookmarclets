7a2fd44f533ae95912e6420f564400ed4bf21388
marc-thorson <marc.thorson@nelnet.net>
2022-08-06 15:44:44 -0500

    Create confluence-strip-to-main.js

---
+++ confluence-strip-to-main.js
@@ -0,0 +1,9 @@
+javascript: (function () {
+    document.querySelector("#likes-and-labels-container").remove();
+    document.querySelector("#comments-section").remove();
+    document.querySelector("#custom-content-footer").remove();
+    document.querySelector("#header").remove();
+    document.querySelector("#full-height-container > div.ia-splitter > div.ia-splitter-left").remove();
+    document.querySelector("#footer").remove();
+    document.querySelector("#main").setAttribute("style", "margin:0");
+})();
