504d7bfe58d974e9ba46cbc4c9dfbc71fa8516ba
marc-thorson <marc.thorson@nelnet.net>
2022-08-07 12:20:35 -0500

    Update confluence-strip-to-main.js

--- confluence-strip-to-main.js
+++ confluence-strip-to-main.js
@@ -2,6 +2,10 @@ javascript: (function () {
     document.querySelector("#likes-and-labels-container").remove();
     document.querySelector("#comments-section").remove();
     document.querySelector("#custom-content-footer").remove();
+    document.querySelector('#breadcrumb-section').remove();
+    document.querySelector('#page-metadata-banner').remove();
+    document.querySelector('#navigation > ul').remove();
+    document.querySelector('#assistive-skip-links').remove();
     document.querySelector("#header").remove();
     document.querySelector("#full-height-container > div.ia-splitter > div.ia-splitter-left").remove();
     document.querySelector("#footer").remove();
